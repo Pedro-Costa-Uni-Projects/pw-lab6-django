ff89c7c4288b7960ea3d918114f1c9fb1bf5d369
Pedro Costa <a21904825@alunos.ulht.pt>
2021-05-12 18:16:38 +0100

    changed no name path to home so it doesnt give error when open

--- website/urls.py
+++ website/urls.py
@@ -5,6 +5,7 @@ from django.urls import path
 app_name = "website"
 
 urlpatterns = [
+    path('', views.home_page_view, name='home'),  # para quando abrir a app o default seja esta
     path('home', views.home_page_view, name='home'),
     path('pag1', views.page_one_view, name='page1'),
     path('pag2', views.page_two_view, name='page2')
